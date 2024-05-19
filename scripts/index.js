e2d96b2a4f901b6973306e4cd58751e02af95294
sramosbernal22 <145167112+sramosbernal22@users.noreply.github.com>
2024-05-19 12:31:54 -0500

    changed date

--- scripts/index.js
+++ scripts/index.js
@@ -4,7 +4,7 @@ const giftbox = document.getElementById('merrywrap');
 const canvasC = document.getElementById('c');
 
 const config = {
-  birthdate: 'May 29, 2001',
+  birthdate: 'May 29, 2024',
   name: 'Leslie'
 };
 
